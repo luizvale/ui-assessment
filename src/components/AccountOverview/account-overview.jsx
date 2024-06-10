3ff3d55bceaf908ce4c445aa74be4c64bba7b29a
Luiz Felipe Silva Vale <luiz.vale@grupobyx.com.br>
2024-06-10 08:56:59 -0300

    add proptypes

--- src/components/AccountOverview/account-overview.jsx
+++ src/components/AccountOverview/account-overview.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './account-overview.css';
 import SupportContact from '../SupportContact/support-contact.jsx';
 import Sales from '../Sales/sales.jsx';
 
 export const AccountOverview = ({ data }) => {
   return (
-    <div class='container-overview'>
+    <div className='container-overview'>
       <div className="account-overview">
         <h2 className='title'>
-            Account Overview
+          Account Overview
         </h2>
-        <SupportContact />
+        <SupportContact contact={data.supportContact} />
       </div>
-      <Sales/>
+      <Sales data={data.salesOverview} />
     </div>
   );
 }
+
+AccountOverview.propTypes = {
+  data: PropTypes.shape({
+    supportContact: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired
+    }).isRequired,
+    salesOverview: PropTypes.shape({
+      uploads: PropTypes.number.isRequired,
+      linesSaved: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default AccountOverview;
